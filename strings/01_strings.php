62caab4ce0f0808869de50740d5271236c54fc07
Elieser Martins <ejmartins81@gmail.com>
2020-11-25 13:34:56 -0300

    strings

---
+++ strings/01_strings.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nome = "Hcode";
+
+    $nome2 = 'Treinamentos';
+
+    //var_dump ($nome, $nome2);
+
+    // Vc consegue colocar variáveis dentro das aspas duplas
+    // com aspas simples ele entende que tudo é texto
+    
+    echo 'ABC $nome';
+
+?>
