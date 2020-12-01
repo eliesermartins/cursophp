e813e17da0063ad145dc85f6b2395eef423d9b67
Elieser Martins <ejmartins81@gmail.com>
2020-12-01 09:59:59 -0300

    strings continuação

--- strings/01_strings.php
+++ strings/01_strings.php
@@ -1,5 +1,6 @@
 <?php
 
+    /*
     $nome = "Hcode";
 
     $nome2 = 'Treinamentos';
@@ -10,5 +11,55 @@
     // com aspas simples ele entende que tudo é texto
     
     echo 'ABC $nome';
+    */
+    /*
+    $nome = "elieser martins";
+
+    $nome = strtoupper($nome);//converte tudo pra maiusculo
+
+    echo $nome;
+    
+    $nome = strtolower($nome);//converte tudo para minusculo
+    
+    echo "<br>";
+
+    echo $nome;
+
+    echo "<br>";
+
+    echo ucfirst($nome);// primeira letra de cada palavra maiuscula
+    */
+
+    /*
+    $nome = "Hcode";
+    echo $nome;
+    echo "<br>";
+    $nome = str_replace("o", "0", $nome);// troca de caracteres
+    $nome = str_replace("e", "3", $nome);
+
+    echo $nome;
+
+    echo "<br>";
+    */
+
+    $frase = "A repetição é a mãe da retenção.";
+
+    $palavra = "mãe";
+
+    $q = strpos($frase, $palavra);
+
+    $texto = substr($frase, 0, $q);
+
+    var_dump($texto);
+
+    echo "<br>";
+
+    $texto2 = substr($frase, $q + strlen($palavra), strlen($frase));
+
+    var_dump($texto2);
+
+    echo "<br>";
+
+    echo $texto . $palavra . $texto2;
 
 ?>
