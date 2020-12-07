5d19e31e40c9adf9e24020ff8ac1005b6e31a197
Elieser Martins <ejmartins81@gmail.com>
2020-12-07 10:41:51 -0300

    conitnuando o aprendizado sobre for, foreach e while

--- for/01_for.php
+++ for/01_for.php
@@ -1,6 +1,6 @@
 <?php
 
-
+/*
 for ($i = 0; $i < 100; $i++) {
 
     if ($i > 50 && $i < 80) continue;
@@ -8,7 +8,15 @@ for ($i = 0; $i < 100; $i++) {
     //if ($i === 900) break;
     
     echo $i . "<br>";
+    */
 
-}
+    echo "<select>";
 
+    for ($i=date("Y"); $i > date("Y") -100 ; $i--) { 
+
+        echo '<option value="' . $i .'">' . $i . '</option>';
+
+    }
+    echo "<select>";
+    
 ?>
